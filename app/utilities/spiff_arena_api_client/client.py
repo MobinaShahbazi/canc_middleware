fbb66766d2202ba9bdf496891bca6aff5162c317
Mehrdad <m.azmin>
2024-09-08 22:48:41 +0330

    feature - updated authorization procedure of the Spiff Client

--- app/utilities/spiff_arena_api_client/client.py
+++ app/utilities/spiff_arena_api_client/client.py
@@ -10,12 +10,7 @@ class SpiffArenaAPIClient:
         self.base_api_url = settings.spiff_arena_base_api_url
         self.base_url = settings.spiff_arena_base_url
         self.project_location = 'canc:test'                   # change
-
-        if not settings.spiff_arena_token:
-            self.token = self.get_token()
-            self.access_token = self.token['access_token']
-        else:
-            self.access_token = settings.spiff_arena_token
+        self.token = self.get_token()
 
         # TODO: Project location or modified_process_group_id should be passed to the client on method calls.
         # project_location = app_config.project_location
@@ -30,18 +25,19 @@ class SpiffArenaAPIClient:
         except Exception as e:
             logging.log(level=logging.ERROR, msg='An error occured in obtaining Spiff Arena token')
             raise
-        # self.token = auth.token
         self.access_token = self.token['access_token']
 
+    @staticmethod
     def check_token(func):
-        def wrapper(self, *args, **kwargs):
+        def wrapped(self, *args, **kwargs):
             if is_token_expired(self.access_token, self.base_api_url):
                 logging.log(level=logging.WARNING, msg="BPMN Service token expired, obtaining a new one.")
                 self.get_token()
+            return func(self, *args, **kwargs)
 
-        return wrapper
+        return wrapped
 
-    # TODO this should be decorated with check_token as soon as getting token directly from spiff arena works
+    @check_token
     def http_request(self, method, url, body=None):
         full_url = f'{self.base_api_url}/{url}'
         request = requests.Request(method, full_url, json=body, headers={'Authorization': self.access_token})
@@ -59,7 +55,7 @@ class SpiffArenaAPIClient:
         results = response.json()
         return results
 
-    def get_process_instances(self, process_model_identifier):
+    def get_process_instances(self, modified_process_model_identifier):
         url = f'process-instances'
         body = {
             "report_metadata":
@@ -67,8 +63,8 @@ class SpiffArenaAPIClient:
                     "columns": [],
                     "filter_by": [
                         {
-                            "field_name": "process_model_identifier",
-                            "field_value": process_model_identifier
+                            "field_name": "modified_process_model_identifier",
+                            "field_value": modified_process_model_identifier
                         }
                     ],
                     "order_by": []
